4312541e4b364312bec6326b140e58c8e80c5002
shali.wang <shali.wang@7x-networks.com>
2021-12-22 17:17:58 +0800

    11111

---
+++ src/main/java/demo.java
@@ -0,0 +1,11 @@
+/**
+ * @Author wsl
+ * @Date 2021-12-22
+ * @Description
+ * @Version 1.0
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("我的世界");
+    }
+}
