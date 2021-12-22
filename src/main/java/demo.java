5bfbb90fa2cb216fb1ad3dee27673d92d90c0801
shali.wang <shali.wang@7x-networks.com>
2021-12-22 17:40:48 +0800

    wsl22222222222

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -8,5 +8,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("我的世界");
         System.out.println("wsl分支");
+        System.out.println("wsl2");
+        System.out.println("wsl2");
     }
 }
