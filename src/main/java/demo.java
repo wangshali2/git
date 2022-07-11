570da90d1b47fe996e12d74613db45332b07598c
shali.wang <shali.wang@7x-networks.com>
2022-07-11 13:17:16 +0800

    wsl  1

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -7,14 +7,10 @@
 public class demo {
     public static void main(String[] args) {
 
-        System.out.println("wsl分支");
+
 
         System.out.println("master");
 
-        System.out.println("wsl分支222222222222");
-        System.out.println("wsl分支333333333333");
-        System.out.println("wsl分支4444444444444");
-        System.out.println("data11111111111111");
 
 
 
