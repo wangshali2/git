5668c5d901288c8c8713c71035441874d81b5f12
shali.wang <shali.wang@7x-networks.com>
2022-07-11 13:26:07 +0800

    wsl  1

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -12,8 +12,7 @@ public class demo {
         System.out.println("master");
 
         System.out.println("wsl分支222222222222");
-        System.out.println("wsl分支333333333333");
-        System.out.println("wsl分支4444444444444");
+
 
     }
 }
